884a868d254a1237e0cdc3d181f3dc36106e0e56
[Aqib abass] <[aqibabass7@gmail.com]>
2024-12-31 17:11:32 +0530

    initial commit

---
+++ src/components/ui/custom/Header.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from '../button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from "@/components/ui/dialog";
+import { FcGoogle } from "react-icons/fc";
+import axios from 'axios';
+
+function Header() {
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  const [openDialog, setOpenDialog] = useState(false);
+
+  useEffect(() => {
+    
+  }, []);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResp) => {
+        const accessToken = codeResp?.access_token;
+        if (accessToken) {
+            GetUserProfile(accessToken);
+        }
+    },
+    onError: (error) => console.error(error),
+});
+
+const GetUserProfile = (accessToken) => {
+  axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`, {
+      headers: {
+          Authorization: `Bearer ${accessToken}`,
+          Accept: 'Application/json'
+      }
+  }).then(resp => {
+      localStorage.setItem('user', JSON.stringify(resp.data)); 
+      setOpenDialog(false);
+      window.location.reload();
+  }).catch(error => {
+      console.error('Error fetching user profile:', error); 
+  });
+};
+
+  return (
+    <div className='w-full p-3 mt-1 shadow-sm flex justify-between items-center px-10'>
+    <a href="/">
+      <img src='/logo.svg' alt="Logo"  />
+    </a>
+      <div>
+        {user ? (
+          <div className=' flex items-center gap-3 '>
+           
+           <a href='/create-trip'>
+            <Button variant="outline" className=" rounded-full">+ Create Trip</Button>
+            </a>
+
+           <a href='/my-trips'>
+            <Button variant="outline" className=" rounded-full">My Trips</Button>
+            </a>
+            <Popover>
+              <PopoverTrigger className=" px-1 gap-2 py-1 bg-transparent  text-white cursor-pointer rounded-full">
+                <img
+                src={user?.picture}
+                alt="User Profile"
+                className=" h-[34px] w-[34px] rounded-full"
+                />
+              </PopoverTrigger>
+              <PopoverContent>
+              <h2 className='cursor-pointer ' onClick={()=>{ 
+                googleLogout(); 
+                localStorage.clear(); 
+                window.location.reload(); 
+                }}>Logout</h2>
+              </PopoverContent>
+            </Popover>
+          </div>
+        ) : (
+          <Button onClick={()=>setOpenDialog(true)}>Sign In</Button>
+        )}
+      </div>
+      <Dialog open={openDialog}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogDescription>
+                            <img src='/logo.svg' />
+                            <h2 className='font-bold text-lg mt-7 flex gap-4 items-center '>Sign In With Google</h2>
+                            <p> Sign in securely using your Google account</p>
+                            <Button
+                                onClick={login}
+                                className='mt-5 w-full'>
+                                <FcGoogle className='h-7 w-7' />
+                                Sign in with google
+                            </Button>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/view-trip/components/InfoSection.jsx
@@ -0,0 +1,55 @@
+import { Button } from '@/components/ui/button'
+import { GetPlaceDetails } from '@/service/GlobalApi';
+import React, { useEffect, useState } from 'react'
+import { IoIosSend } from "react-icons/io";
+
+
+const PHOTO_REF_URL = 'https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=1000&maxWidthPx=1000&key=' + import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
+
+function InfoSection({ trip }) {
+    const [photoUrl,setPhotoUrl]=useState();
+    useEffect(()=>{
+        trip&&GetPlacePhoto();
+
+    },[trip])
+
+    const GetPlacePhoto=async()=>{
+        const data ={
+            textQuery:trip?.userSelection?.location?.label
+        }
+        const result=await GetPlaceDetails(data).then(resp=>{
+            console.log(resp.data.places[0].photos[9].name);
+            const photoUrl=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[9].name);
+            setPhotoUrl(photoUrl)
+        
+        })
+    }
+    return (
+        <div>
+            <img   src={photoUrl || '/placeholder.jpg'} className='h-[340px] w-full object-cover rounded-xl' alt="placeholder" />
+            <div className= 'flex justify-between items-center'>
+
+
+                <div className='my-5 flex flex-col gap-2'>
+                    <h2 className='font-bold text-2xl'>{trip?.userSelection?.location?.label}</h2>
+                    <div className=' flex gap-1'>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sm md:text-md'>
+                            📅 {trip.userSelection?.noOfDays} Day
+                        </h2>
+
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sm md:text-md'>
+                            💰 {trip.userSelection?.budget} Budget
+                        </h2>
+
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sm md:text-md'>
+                            🧳 No. of Travellers: {trip.userSelection?.traveller}
+                        </h2>
+                    </div>
+                </div>
+                <Button > <IoIosSend /> </Button>
+            </div>
+        </div>
+    )
+}
+
+export default InfoSection
